bcbbf692905909da85b8b04a17a9d5c8f7cd3b15
sorxcode <sorxcode@gmail.com>
2022-03-19 00:29:45 +0100

    checkpoint

---
+++ contracts/Demo2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Demo2 {
+    string public secret = "i-dont-konw";
+    address public owner = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
+
+    function setSecret(string memory _s) public {
+    }
+}
